34dc408cddc26b2fdd472b6ed8f5a687a0e39ce0
Zain Jamshaid <zainjamshaid25@gmail.com>
2025-08-07 18:57:44 +0500

    Created Contact Page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,15 @@
+import ContactSection from "../components/ContactSection"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+
+
+export default () => {
+    return(
+    <div>
+        <Navbar />
+        <ContactSection />
+        <Footer />
+    </div>
+    )
+}
