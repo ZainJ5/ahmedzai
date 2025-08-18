bc3c658ac22fdfc95e119f9634b8c8a1d166d4fa
Zain Jamshaid <zainjamshaid25@gmail.com>
2025-08-18 14:15:00 +0500

    Whatsapp message fix

--- src/app/products/components/ProductCard.jsx
+++ src/app/products/components/ProductCard.jsx
@@ -10,7 +10,7 @@ export default function ProductCard({ product }) {
   const handleWhatsAppClick = (e) => {
     e.stopPropagation();
     e.preventDefault();
-    const message = encodeURIComponent(`Hello, I'm interested in the ${product.title} (${product.model}) Link:https://ahmadzaitrading.com/products/${product._id}.`);
+    const message = encodeURIComponent(`Hello, I'm interested in the ${product.title} (${product.model}) Link:https://ahmadzaitrading.com/products/${product._id}`);
     window.open(`https://wa.me/+818046646786?text=${message}`, '_blank');
   };
 
